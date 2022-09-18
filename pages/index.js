83421b38ef4dd3dc28e6d8cab954e65e29149e79
Nailul Ngafwa <fakea4988@gmail.com>
2022-09-18 19:27:09 +0700

    update component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-
-import NavBar from '@components/NavBar';
-import Features from '@components/Features';
+import NavBar from "../components/NavBar";
+import Features from "../components/Features";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen text-white">
+    <div className="min-h-screen bg-gradient-to-b from-gray-600 to-gray-900 text-white">
       <NavBar />
       <div className="container mx-auto">
         <Features />
       </div>
     </div>
-  )
+  );
 }
